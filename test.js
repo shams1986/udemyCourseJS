1fdbd2f47ccff51ce0adc091cb28a3fa3bd23b71
shams <example@gmail.com>
2021-11-13 17:28:49 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,32 @@
+"use strict";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//interpolyaciya
+const category = 'toys';
+console.log(`https://someurl.com/${category}`);
+
+*/
+
+/*
+//promt()
+let arr = [];
+arr[0] = prompt('name: ', '');
+arr[1] = prompt('lastname: ', '');
+console.log(arr);
+*/
