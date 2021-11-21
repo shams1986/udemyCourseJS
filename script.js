7aef3ab13f44c09cca104fa266ccab73c389be86
shams <example@gmail.com>
2021-11-21 18:37:51 +0100

    first

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 "use strict";
+
+const numberOfFilms = +prompt("wieviel filmen hast du angesehen ?","");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movie: {},
+    actors:{},
+    genres:[],
+    privat: false
+
+};
+
+const a = prompt("Eine zu letzt gesegene film ?",""),
+      b = prompt("bewerte diese film"),
+      c = prompt("Eine zu letzt gesegene film ?",""),
+      d = prompt("bewerte diese film");
+
+
+personalMovieDB.movie[a] = b;
+personalMovieDB.movie[c] = d;
+
+console.log(personalMovieDB);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
