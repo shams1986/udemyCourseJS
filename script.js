cc4884ea5c9089a3a7a60a8050884954eeb311f6
shams <example@gmail.com>
2021-12-09 21:24:47 +0100

    urok 23 osnovi oop

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 
-/* 
+
 let numberOfFilms;
 
 function start() {
@@ -80,7 +80,7 @@ function writeYourGenres() {
 
 writeYourGenres();
 
-
+/*
 function learnJS(lang, callback) {
     console.log(`ya uchu ${lang}`);
     callback();
@@ -89,9 +89,9 @@ function learnJS(lang, callback) {
 
 learnJS('JavaScript', function () {
     console.log('ya proshol etot urok');
-}); */
+}); 
 
-/* 
+
 
 UROK 20: Objecti
 
@@ -117,7 +117,7 @@ options.makeTest();
 
 
 for (let key in options) {
-    if (typeof (options[key]) == 'object') {
+    if (typeof(options[key]) == 'object') {
         for (let i in options[key]) {
             console.log(`${options[key][i]}`);
         }
@@ -126,13 +126,19 @@ for (let key in options) {
     }
 } */
 
-const arr = [1, 2, 3, 4, 6, 8];
+/* const arr = [1, 2, 32, 7, 6, 8];
 
-const str = prompt(", ");
-const products = str.split(', ');
-console.log(products);
+//metod sort prinimaet 'callback' function
+arr.sort(compareNum);
 
-/* arr.pop();
+//sorteruet int array po vozrastaniyu
+function compareNum(a, b) {
+    return a - b;
+}
+
+console.log(arr); 
+
+ arr.pop();
 console.log(arr);
 
 arr.push(22);
@@ -157,8 +163,101 @@ for(let value of arr){
 arr.forEach(function (item, i, arr) {
     console.log(`index ${i} : ${item} vom array ${arr}`);
 });
+ 
+
+
+
+//razbivaet stroku i pomeshaet ego v array
+ const str = 'bbb, eee, sss, aaa';
+const products = str.split(', ');
+console.log(products);
+// soedinyaet elemnti array i vidaet v odnoi stroke
+console.log(products.join('; '));
+
+products.sort();
+console.log(products); */
+
+/*
+Urok 22: 
+
+const obj = {
+    a: 4,
+    b: 6,
+    objIn: {
+        x: 10,
+        y: 20
+    }
+};
+
+const add = {
+    e: 300,
+    h: 400
+};
+
+// 2 objects join
+const added = Object.assign(obj, add);
+
+// make first level(poverhtnostniy) copy from object obj 
+const clone = Object.assign({}, obj);
+
+
+const oldArr = ['a', 'b', 'c'];
+//array clone
+const newArr = oldArr.slice();
+newArr[1] = 'bbb';
+
+
+// 'spread' operator( operator razvorota)
+const video = ['youtube', 'vimeo', 'rutube'],
+    blog = ['wordpress', 'livejournal', 'blogger'];
+
+//operator '...bread' peredaet elementi massiva video kak otdelnie elementi
+const internet = [...video, ...blog, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const logArr = [4,3,6];
+log(...logArr); 
+log(logArr[0],logArr[1],logArr[2]); 
+*/
+
+/* 
+// urok 23: osnovi oop: prototipno orientirovannoe nasledovanie
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function(){
+        console.log('Hello');
+    }
+};
+
+
+const john = {
+    health: 200
+};
+
+//metod '__proto__'  depracate(ustarel i ne ispolzuetsya)
+john.__proto__ = soldier;
+console.log(john.armor);
+
+
+//seichas vmesto '__proto__' nujno ispolzovat takie komandi kak
+//Object.create(), Object.getPrototypeOf() i Object.setPrototypeOf()
+
+//ustanavlivaet protype dlya 'john' ot soldata
+Object.setPrototypeOf(john,soldier); //<--> john.__proto__ = soldier;
+john.sayHello();
+
+//Sozdaetsya noviy object 'mike' kotoriy budet prototipna nasledovatsya ot 'soldier'
+const mike = Object.create(soldier);
+mike.sayHello();
+
+
  */
-
-
-
-
